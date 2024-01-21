b9a7df8bcd8028e01a58b5b58a36ce30f9d7b4c6
Captain Crypto <tamir@crypto-knight.tech>
2024-01-21 23:11:38 +0200

    user manager

---
+++ src/aime-api/aime.ts
@@ -0,0 +1,7 @@
+import * as env from './env'
+import * as users from './user-manager'
+
+export {
+    env,
+    users
+}
--- src/xapp.ts
+++ src/xapp.ts
@@ -11,7 +11,7 @@ import {
     XUI, XData as _XD, _xem, Wormholes, WormholeEvents
 } from 'xpell'
 
-import * as AimeAPI from './aime-api/env'
+import * as AimeAPI from './aime-api/aime'
 import "/public/style/aime.css"
 
 
@@ -24,17 +24,13 @@ const main = async () => {
     _x.start() // start Xpell frame counter
 
 
-    const wormholeServer = "127.0.0.1:3030"
+    const wormholeServer = "localhost:3030"
     const wormholeUrl = "ws://" + wormholeServer + "/" // wormhole server url (localhost)
 
 
 
 
 
-    // _xem.on(WormholeEvents.WormholeOpen, async (e) => {
-    //     const res = await Wormholes.sendSync(AimeAPI._get_environment_name)
-    //     console.log(res)
-    // })
 
     Wormholes.open(wormholeUrl)
 
@@ -87,6 +83,38 @@ const main = async () => {
                 _children: [
                     {
                         _type: "view",
+                        _id: "main-content-buttons-bar",
+                        class: "main-content-buttons-bar",
+                        _children: [
+                            {
+                                _type: "button",
+                                _id: "btn-get-environment-name",
+                                class: "main-content-button",
+                                _text: "Get Environment Name",
+                                _on_click: async (xobj, e) => {
+                                    (<any>XUI.getObject("main-content-command-bar-input")).dom.value = JSON.stringify(AimeAPI.env._get_environment_name)
+                                }
+                            },
+                            {
+                                _type: "button",
+                                class: "main-content-button",
+                                _text: "Add User",
+                                _on_click: async (xobj, e) => {
+                                    (<any>XUI.getObject("main-content-command-bar-input")).dom.value = JSON.stringify(AimeAPI.users._add_user)
+                                }
+                            },
+                            {
+                                _type: "button",
+                                class: "main-content-button",
+                                _text: "Auth User",
+                                _on_click: async (xobj, e) => {
+                                    (<any>XUI.getObject("main-content-command-bar-input")).dom.value = JSON.stringify(AimeAPI.users._auth_user)
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        _type: "view",
                         _id: "main-content-command-bar",
                         class: "main-content-command-bar",
                         _children: [
@@ -100,7 +128,38 @@ const main = async () => {
                                 _type: "textarea",
                                 _id: "main-content-command-bar-input",
                                 class: "main-content-command-bar-item",
+                                style: "height: 200px;",
                                 _text: "request"
+                            },
+                            {
+                                _type: "view",
+                                _id: "main-content-command-bar-send",
+                                class: "main-content-command-bar-send",
+                                _children: [
+                                    {
+                                        _type: "button",
+                                        class: "main-content-button",
+                                        _text: "Send",
+                                        _on_click: async (xobj, e) => {
+                                            const req = JSON.parse((<any>XUI.getObject("main-content-command-bar-input")).dom.value)
+                                            let res = ""
+                                            try {
+                                                res = await Wormholes.sendSync(req);
+                                                console.log(res)
+                                                if(typeof res == "object") {
+                                                    res = JSON.stringify(res, null, 2)
+                                                } 
+                                            } catch (e) {
+                                                res = e
+                                            }
+                                            (<any>XUI.getObject("main-content-command-bar-send")).setText(res)
+
+                                        }
+                                    }
+                                ]
+
+
+
                             },
                             {
                                 _type: "view",
@@ -109,31 +168,15 @@ const main = async () => {
                                 _text: "Response"
                             },
                             {
-                                _type: "view",
+                                _type: "xhtml",
+                                _html_tag:"pre",
                                 _id: "main-content-command-bar-send",
                                 class: "main-content-command-bar-item",
                                 _text: "Response"
                             }
                         ]
                     },
-                    {
-                        _type: "view",
-                        _id: "main-content-buttons-bar",
-                        class: "main-content-buttons-bar",
-                        _children: [
-                            {
-                                _type: "button",
-                                _id: "btn-get-environment-name",
-                                class: "main-content-button",
-                                _text: "Get Environment Name",
-                                _on_click: async (xobj, e) => {
-                                    XUI.getObject("main-content-command-bar-input").dom.value = JSON.stringify(AimeAPI._get_environment_name)
-                                    const res = await Wormholes.sendSync(AimeAPI._get_environment_name)
-                                    XUI.getObject("main-content-command-bar-send").setText(res)
-                                }
-                            }
-                        ]
-                    }
+
                 ]
             }
         ]
